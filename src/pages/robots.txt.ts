37015ec92b1a3e339a444b84bbc564d6b2a90a38
Rigler Zsolt <zsolt.rigler@icloud.com>
2026-06-27 19:59:18 +0200

    chore: exclude /ui from sitemap and robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,7 +3,7 @@ import { SITE } from "../config"
 
 export const GET: APIRoute = () => {
   return new Response(
-    ["User-agent: *", "Allow: /", "", `Sitemap: ${SITE.url}/sitemap-index.xml`].join("\n"),
+    ["User-agent: *", "Allow: /", "Disallow: /ui/", "", `Sitemap: ${SITE.url}/sitemap-index.xml`].join("\n"),
     { headers: { "Content-Type": "text/plain; charset=utf-8" } }
   )
 }
